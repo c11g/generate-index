7ce8b8c449db7431474eb7b333fe288256c66a27
choyoungkwang <nt99108@nhn.com>
2015-10-23 18:45:53 +0900

    folder 별 grouping 작업 진행

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var gulpIndxing = function(setting) {
 		'author': setting.author || '작업자를 입력하세요.',
 		'srcDir': setting.srcDir || 'demo/',
 		'outDir': setting.outDir || '',
+		'extention': setting.extention || '.html',
 		'outFileName': setting.outFileName || '@index.html'
 	}
 
@@ -22,42 +23,72 @@ var gulpIndxing = function(setting) {
 		author = option.author,
 		srcDir = option.srcDir,
 		outDir = option.outDir,
+		extention = option.extention,
 		outFileName = option.outFileName;
 
-	// html 과 page title 리스트
-	var htmlList = [],
-		titleList = [];
-	
-	getHtmlList(srcDir);
-	getTitleList(htmlList, srcDir);
+	// pageList 와 page title 리스트
+	var pageList = {
+			'base': [] // 폴더 없는 경우 기본 배열
+		},
+		titleList = {
+			'base': []
+		};
+
+	getList(srcDir);
 	createIndex(outDir + outFileName);
 
-	// html 리스트 추출
-	function getHtmlList(srcDir){
-		var _allFiles = fs.readdirSync(srcDir);
+	// 페이지, 타이틀 리스트 추출
+	function getList(srcDir){
 
-		for(var index in _allFiles){
-			var _list = _allFiles[index];
+		var _all = fs.readdirSync(srcDir);
+		// srcDir 탐색
+		for (var index in _all){
+			var _list = _all[index];
 
-			// html파일인지 확인 후 htmlList에 추가
-			var _check = _list.indexOf('.html');
-
-			if( _check !== -1 ) {
-				htmlList.push(_list);
-			}
-		}
-	}
-
-	// page title 추출
-	function getTitleList(htmlList, srcDir){
-		for ( var i=0 ; i < htmlList.length ; i++ ) {
-			var _data = fs.readFileSync(srcDir + htmlList[i], 'utf-8');
-
-			// <title> 추출 후 titleList에 추가
-			var pattern = /<title>([\s\S]*)<\/title>/i,
-				title = pattern.exec(_data)[1];
+			// 폴더인지 파일인지 확인
+			var _checkType = fs.statSync(srcDir + _list);
+			
+			if (_checkType.isDirectory()){
+				// 폴더이면..
+				// 우선 pageList에 폴더이름의 배열을 생성
+				pageList[_list] = [],
+				titleList[_list] = [];
 				
-				titleList.push(title);
+				var _pages = fs.readdirSync(srcDir + _list);
+				// 해당 폴더를 탐색
+				for (var index in _pages){
+					// 확장자를 확인 후 배열에 추가
+					var _checkExtention = _pages[index].indexOf(extention);
+
+					if( _checkExtention !== -1 ) {
+						pageList[_list].push(_pages[index]);
+
+						// <title> 추출
+						var _data = fs.readFileSync(srcDir + _list + '/' + _pages[index], 'utf-8');
+						var pattern = /<title>([\s\S]*)<\/title>/i,
+							title = pattern.exec(_data)[1];
+
+						// titleList 배열에 추가
+						titleList[_list].push(title);
+					}
+				}
+			} else if (_checkType.isFile()){
+				// 파일이라면..
+				// 확장자를 확인 후 기본배열에 추가
+				var _checkExtention = _list.indexOf(extention);
+
+				if (_checkExtention !== -1){
+					pageList.base.push(_list);
+
+					// <title> 추출
+					var _data = fs.readFileSync(srcDir + _list, 'utf-8');
+					var pattern = /<title>([\s\S]*)<\/title>/i,
+						title = pattern.exec(_data)[1];
+
+					// titleList 기본 배열에 추가
+					titleList.base.push(title);
+				}
+			}
 		}
 	}
 
@@ -89,22 +120,24 @@ var gulpIndxing = function(setting) {
 					_t.getHours() + '시 ' +
 					( _t.getMinutes() < 10 ? '0' : '' ) + _t.getMinutes() + '분 수정됨';
 
-		// 마지막 수정
-		$('#date').text(time)
+		// 마지막 수정시간 출력
+		$('#date').text(time);
 		
-		// 인덱스 리스트
-		for (var i = 0; i < htmlList.length; i++ ) {
-			$('#list').append('<li><a href='+ srcDir + htmlList[i] +'>' + titleList[i] + '<span>/ ' + htmlList[i] + '</span>' + '</a></li>\n');
+		// 리스트 작성
+		for(var key in pageList){
+			for (var i = 0; i < pageList[key].length; i++){
+				$('#list').append('<li><a href='+ srcDir + pageList[key][i] +'>' + titleList[key][i] + '<span>/ ' + pageList[key][i] + '</span>' + '</a></li>\n');
+			}
 		}
 
 		// 변경 내용 쓰기
-		fs.writeFileSync(file, $.html(), 'utf-8')
+		fs.writeFileSync(file, $.html(), 'utf-8');
 	}
 }
 
 module.exports = gulpIndxing;
 
-// gulpIndxing({
-//     'projectName': '네이버',
-//     'author': '메시'
-// });
+gulpIndxing({
+    'projectName': '네이버',
+    'author': '메시'
+});
