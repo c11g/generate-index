e5c3d74e16dba4f5600c2bfae1fe62aa11eba2c7
choyoungkwang <choyoungkwang@nts-corp.com>
2020-11-20 01:24:18 +0900

    code refactoring

--- index.js
+++ index.js
@@ -1,146 +1,68 @@
-'use strict';
+const fs = require("fs");
+const path = require("path");
+const cheerio = require("cheerio");
 
-var fs = require('fs'),
-	path = require('path'),
-	cheerio = require('cheerio');
-
-// 사용자 옵션
-var setting = {};
-
-var generateIndxing = function(setting) {
-
-	// 사용자 옵션 정의 || 기본값
-	var option = {
-		'projectName': setting.projectName || '프로젝트명을 입력하세요',
-		'author': setting.author || '작업자를 입력하세요.',
-		'srcDir': setting.srcDir || 'demo/',
-		'extention': setting.extention || '.html',
-		'outFileName': setting.outFileName || '@index.html'
-	}
-
-	var projectName = option.projectName,
-		author = option.author,
-		srcDir = option.srcDir,
-		extention = option.extention,
-		outFileName = option.outFileName;
-
-	// pageList 와 page title 리스트
-	var pageList = {
-			'base': [] // 폴더 없는 경우 기본 배열
-		},
-		titleList = {
-			'base': []
-		};
-
-	getList(srcDir);
-	createIndex(srcDir + outFileName);
-
-	// 페이지, 타이틀 리스트 추출
-	function getList(srcDir){
-
-		var _all = fs.readdirSync(srcDir);
-		// srcDir 탐색
-		for (var index in _all){
-			var _list = _all[index];
-
-			// 폴더인지 파일인지 확인
-			var _checkType = fs.statSync(srcDir + _list);
-			
-			if (_checkType.isDirectory()){
-				// 폴더이면..
-				// 우선 pageList에 폴더이름의 배열을 생성
-				pageList[_list] = [],
-				titleList[_list] = [];
-				
-				var _pages = fs.readdirSync(srcDir + _list);
-				// 해당 폴더를 탐색
-				for (var index in _pages){
-					// 확장자를 확인 후 배열에 추가
-					var _checkExtention = _pages[index].indexOf(extention);
-
-					if( _checkExtention !== -1 ) {
-						pageList[_list].push(_pages[index]);
-
-						// <title> 추출
-						var _data = fs.readFileSync(srcDir + _list + '/' + _pages[index], 'utf-8');
-						var pattern = /<title>([\s\S]*)<\/title>/i,
-							title = pattern.exec(_data)[1];
-
-						// titleList 배열에 추가
-						titleList[_list].push(title);
-					}
-				}
-			} else if (_checkType.isFile()){
-				// 파일이라면..
-				// 확장자를 확인 후 기본배열에 추가
-				var _checkExtention = _list.indexOf(extention);
-
-				if (_checkExtention !== -1){
-					pageList.base.push(_list);
-
-					// <title> 추출
-					var _data = fs.readFileSync(srcDir + _list, 'utf-8');
-					var pattern = /<title>([\s\S]*)<\/title>/i,
-						title = pattern.exec(_data)[1];
-
-					// titleList 기본 배열에 추가
-					titleList.base.push(title);
-				}
-			}
-		}
-	}
-
-	// @index.html
-	function createIndex(file){
-		// htmlTemplate 파일 참조
-		var templatePath = path.join(__dirname, 'template/@temp.html'),
-			htmlTemplate = fs.readFileSync(templatePath, 'utf-8');
-
-		// @index.html에 템플릿 코드 생성
-		fs.writeFileSync(file, htmlTemplate, 'utf8');
-
-		// DOM 내용 변경
-		var $ = cheerio.load(htmlTemplate, {
-			decodeEntities: false // 한글 인코딩 문제
-		});
-
-		// 프로젝트명
-		$('#title').text(projectName);
-
-		// 작업자
-		$('#admin').text('작업자 : ' + author);
-
-		// 현재 시간
-		var _t = new Date(),
-			time = _t.getFullYear() + '년 ' +
-					(_t.getMonth() + 1) + '월 ' + 
-					_t.getDate() + '일 ' +
-					_t.getHours() + '시 ' +
-					( _t.getMinutes() < 10 ? '0' : '' ) + _t.getMinutes() + '분 수정됨';
-
-		// 마지막 수정시간 출력
-		$('#date').text(time);
-		
-
-		// 리스트 작성
-		for(var key in pageList){
-			var _name;
-			key === 'base' ? _name = '' : _name = key+'/';
-			if(pageList[key] != '') $('.content').append('<h2>' + _name + '</h2>').append('<ul class="list ' + key + '"></ul>'); 
-			for (var i = 0; i < pageList[key].length; i++){
-				$('.'+ key).append('<li><a href='+ _name +  pageList[key][i] +'>' + titleList[key][i] + '<span>: ' + _name +  pageList[key][i] + '</span>' + '</a></li>\n');
-			}
-		}
-
-		// 변경 내용 쓰기
-		fs.writeFileSync(file, $.html(), 'utf-8');
-	}
+const getTitle = filepath => {
+	const data = fs.readFileSync(filepath, "utf-8");
+	const pattern = /<title>([\s\S]*)<\/title>/i;
+	return pattern.exec(data)[1];
 }
 
-module.exports = generateIndxing;
+const render = (projectName, author, srcDir, output) => {
+	const htmlTemplate = fs.readFileSync(path.join(__dirname, "template/@temp.html"), "utf-8");
+	fs.writeFileSync(output, htmlTemplate, "utf8");
+
+	const $ = cheerio.load(htmlTemplate, { decodeEntities: false });
+	$("#title").text(projectName);
+	$("#admin").text("작업자 : " + author);
+
+	const date = new Date();
+	const time = `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일 ${date.getHours()<10 ? `0${date.getHours()}` : date.getHours()}:${date.getMinutes()<10 ? `0${date.getMinutes()}` : date.getMinutes()} 수정됨`;
+	$("#date").text(time);
+	
+	for (const folder in data) {
+		$(".content").append(`<h2>${folder}</h2>`);
+		$(".content").append(`<ul class="list">${
+			data[folder].reduce((acc, menu) => acc + `<li><a href="./${folder.slice(srcDir.length)}${menu[0]}">${menu[1]}<span>:${menu[0]}</span></a></li>`, "")
+		}</ul>`);
+	}
+	fs.writeFileSync(output, $.html(), "utf-8");
+}
+
+const scrap = (srcDir, ext) => {
+	data[srcDir] = [];
+	fs.readdirSync(srcDir, {withFileTypes: true}).forEach(item => {
+		if (item.isDirectory()) {
+			const dir = item.name;
+			scrap(`${srcDir}${dir}/`, ext);
+		} else if (item.isFile()) {
+			const name = item.name;
+			if (name.indexOf(ext) === -1) return;
+			const title = getTitle(`${srcDir}${name}`);
+			data[srcDir].push([name, title]);
+		}
+	});
+}
+
+const indexList = ({
+	srcDir = null,
+	ext = ".html",
+	projectName = "프로젝트명을 입력하세요",
+	author = "작업자를 입력하세요.",
+	output = "@index.html"
+}) => {
+	if (!srcDir) return console.log("\"srcDir\"이 없습니다.");
+	scrap(srcDir, ext);
+	render(projectName, author, srcDir, `${srcDir}${output}`);
+}
+
+const data = {};
+
+module.exports = indexList;
 
 // 테스트용 코드
-// generateIndxing({
-//     'projectName': '네이버',
-//     'author': '메시'
-// });
+indexList({
+	"projectName": "Enter a project",
+	"author": "Enter a author",
+	"srcDir": "demo/"
+});
